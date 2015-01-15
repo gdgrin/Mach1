12e464b2dd165c105d143a4b815b0d1d0d928bb7
Gene Grinberg <Gene@blss-MacBook-Pro.local>
2015-01-14 19:10:42 -0500

    Assignment Created

---
+++ Mach1/AssignmentCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AssignmentCellTableViewCell.swift
+//  Mach1
+//
+//  Created by Gene Grinberg on 1/14/15.
+//  Copyright (c) 2015 Gene Grinberg. All rights reserved.
+//
+
+import UIKit
+
+class AssignmentCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Mach1/SampleData.swift
@@ -0,0 +1,9 @@
+//
+//  SampleData.swift
+//  Mach1
+//
+//  Created by Gene Grinberg on 1/13/15.
+//  Copyright (c) 2015 Gene Grinberg. All rights reserved.
+//
+
+import Foundation
