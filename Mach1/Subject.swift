73b62b49aad90ebb5cd0056e08eba860cce52f74
Gene Grinberg <Gene@blss-MacBook-Pro.local>
2015-02-05 18:28:16 -0500

    Added Subject and Subject Button

---
+++ Mach1/Subject.swift
@@ -0,0 +1,13 @@
+//
+//  Subject.swift
+//  Mach1
+//
+//  Created by Gene Grinberg on 2/4/15.
+//  Copyright (c) 2015 Gene Grinberg. All rights reserved.
+//
+
+import UIKit
+
+class Subject: NSObject {
+   
+}
---
+++ Mach1/SubjectButton.swift
@@ -0,0 +1,21 @@
+//
+//  SubjectButton.swift
+//  Mach1
+//
+//  Created by Gene Grinberg on 2/5/15.
+//  Copyright (c) 2015 Gene Grinberg. All rights reserved.
+//
+
+import UIKit
+
+class SubjectButton: UIButton {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
